afaa701b02009e6a29b9f26d61cb35b871119181
Jakub Badowski <badowski@inig.pl>
2016-09-14 18:08:43 +0200

    first full working version

--- web/index.php
+++ web/index.php
@@ -50,4 +50,68 @@ $app->get('/drag', function() use($app) {
     return $app['twig']->render('drag.twig', ['taskList' => $taskList]);
 }); 
 
+// Services
+
+use Symfony\Component\HttpFoundation\Request;
+$app->post('/save-changes', function(Request $request) use ($app) {
+
+	// Data from AJAX
+	// $inputData = $request->get('list');
+
+	$user1 = [];
+	parse_str($_POST['user1'], $user1);
+
+	// echo "user 1 = ";
+	// print_r($user1);
+
+
+	$user2 = [];
+	parse_str($_POST['user2'], $user2);
+
+	// echo "user 2 = ";
+	// print_r($user2);
+
+	$user3 = [];
+	parse_str($_POST['user3'], $user3);
+
+	// echo "user 3 = ";
+	// print_r($user3);
+
+
+	// Update DB
+
+	try {
+		// Clear table
+		$sql = "TRUNCATE TABLE user_task";
+		// $app['db']->executeUpdate($sql);
+		$stmt = $app['db']->prepare($sql);
+		$stmt->execute();
+
+
+		// Add new racords
+		foreach ($user1['task'] as $item) {
+			$app['db']->insert('user_task', [ 'user_id' => 1, 'task_id' => (int) $item ]);
+		}
+
+		foreach ($user2['task'] as $item) {
+			$app['db']->insert('user_task', [ 'user_id' => 2, 'task_id' => (int) $item ]);
+		}
+
+		foreach ($user3['task'] as $item) {
+			$app['db']->insert('user_task', [ 'user_id' => 3, 'task_id' => (int) $item ]);
+		}
+
+    	// $app['db']->fetchAll("SELECT * from user1");
+
+		
+	} catch (Exception $e) {
+		return $app->json( ['status' => 'fail', 'message' => 'sth wrong on server side !'] );
+	}
+
+
+	$outputData = ['status' => 'ok', 'test' => $user1];
+
+	return $app->json($outputData);
+});
+
 $app->run(); 
